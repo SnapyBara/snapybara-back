13230ebcb1ed646dee46ef0ef253a8f9e0d9c4ef
LURZU <tommosti1@gmail.com>
2025-07-01 08:00:20 +0200

    feats: add supabase auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { validateEnvironment } from './config/env.validation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnvironment,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
